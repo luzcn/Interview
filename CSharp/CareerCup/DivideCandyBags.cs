2cf530b1bd71019730993024e102d24bd5bb1b0e
Zheng Lu <luzcn@hotmail.com>
2015-04-03 19:52:03 -0700

    Need to finishe Candy divide DP solution

---
+++ CSharp/CareerCup/DivideCandyBags.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.CareerCup
+{
+    class DivideCandyBags
+    {
+        public int divide(int[] bags)
+        {
+            if (bags.Length == 0)
+                return 0;
+
+            if (bags.Length == 1)
+                return bags[0];
+
+            if (bags.Length == 2)
+                return Math.Abs(bags[0] - bags[1]);
+
+            return 0;
+        }
+
+        private int divideRec(int[] bags, int sum1, int sum2, int index)
+        {
+            if (index >= bags.Length)
+                return Math.Abs(sum1 - sum2);
+
+
+
+            return Math.Min(divideRec(bags, sum1 + bags[index], sum2, index + 1), divideRec(bags, sum1, sum2 + bags[index], index + 1));
+        }
+    }
+}
