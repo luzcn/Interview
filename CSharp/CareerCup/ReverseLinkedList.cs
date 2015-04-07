1f30dff8e3f6ec5357493c70f3ab7b9cabef561b
luzcn <luzcn@hotmail.com>
2015-04-07 15:22:03 -0700

    Need to implement the Combination problem

---
+++ CSharp/CareerCup/ReverseLinkedList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.CareerCup
+{
+    using DataStructure;
+    class ReverseLinkedList
+    {
+        public ListNode reverse(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            return reverse_rec(head, null);
+        }
+
+        /// <summary>
+        /// swap the node in each two pairs
+        /// i.e. 1->2->3->4->5 ==> 2->1->4->3->5
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode swapInPair(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            var p = head;
+            int size = 0;
+            while (p != null)
+            {
+                size++;
+                p = p.next;
+            }
+
+            return reverseKGroup(head, 3, size);
+        }
+
+        private ListNode reverseKGroup(ListNode ptr, int k, int size)
+        {
+            if (ptr == null || size < k)
+                return ptr;
+
+            int count = 0;
+            ListNode pre = null;
+            ListNode current = ptr;
+
+            while (ptr != null && count < k)
+            {
+                var temp = ptr.next;
+                ptr.next = pre;
+                pre = ptr;
+                ptr = temp;
+                count++;
+            }
+            current.next = reverseKGroup(ptr, k, size - k);
+
+            return pre;
+        }
+
+        /// <summary>
+        /// Reverse the whole linked list in recursive.
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="pre"></param>
+        /// <returns></returns>
+        private ListNode reverse_rec(ListNode p, ListNode pre)
+        {
+            if (p == null)
+                return pre;
+
+            var temp = p.next;
+            p.next = pre;
+
+            return reverse_rec(temp, p);
+        }
+
+        /// <summary>
+        /// Reverse in none recursive.
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        private ListNode reverse_NonRec(ListNode head)
+        {
+            var p = head;
+            ListNode pre = null;
+
+            while (p != null)
+            {
+                var temp = p.next;
+                p.next = pre;
+                pre = p;
+                p = temp;
+            }
+
+            return pre;
+        }
+    }
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -8,10 +8,33 @@ namespace CSharp
 {
     using LeetCode;
     using CareerCup;
+    using DataStructure;
     class Program
     {
         static void Main(string[] args)
         {
+            ListNode n1 = new ListNode(1);
+            ListNode n2 = new ListNode(2);
+            ListNode n3 = new ListNode(3);
+            ListNode n4 = new ListNode(4);
+            ListNode n5 = new ListNode(5);
+
+            n1.next = n2;
+            n2.next = n3;
+            n3.next = n4;
+            n4.next = n5;
+
+            ReverseLinkedList rl = new ReverseLinkedList();
+            //var t = rl.reverse(n1);
+            var t = rl.swapInPair(n1);
+
+            while (t != null)
+            {
+                Console.Write(t.val + " ");
+                t = t.next;
+            }
+
+            Console.WriteLine();
         }
     }
 }
