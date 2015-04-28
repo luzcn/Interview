54fa1e62b53f6f91ff43c8652f9bc34b6fa5f823
luzcn <luzcn@hotmail.com>
2015-04-28 11:50:19 -0700

    Need to implement Construct Tree problem.

--- CPP/CPP.cpp
+++ CPP/CPP.cpp
@@ -2,13 +2,31 @@
 //
 
 #include "stdafx.h"
-#include "leetcode\ExcelSheetColumnTitle.h"
+#include "CareerCup\ConstructTreefromInorderandPreorder.h"
+
+
 using namespace std;
 
+
+void inorderT(TreeNode* root)
+{
+    if (!root)
+        return;
+
+    inorderT(root->left);
+    cout << root->val;
+    inorderT(root->right);
+}
 int _tmain(int argc, _TCHAR* argv [])
 {
-    cout << ExcelSheetColumnTitle::convertToTitle(52) << endl;
+    // inorder: 4,2,5,1,6,3,9,7  preorder: 1,2,4,5,3,6,7,9
 
+    vector<int> inorder = { 4, 2, 5, 1, 6, 3, 9, 7 };
+    vector<int> preorder = { 1, 2, 4, 5, 3, 6, 7, 9 };
+
+    auto t = ConstructTreeFromInorderAndPostorder::constructTree(inorder, preorder);
+
+    inorderT(t);
     return 0;
 }
 
