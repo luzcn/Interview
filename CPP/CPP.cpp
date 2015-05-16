56d184bcbc86f476bb794ae2d8aba2040165ab32
Zheng Lu <luzcn@hotmail.com>
2015-05-15 20:59:57 -0700

    Need to finish the Sudoku solver.

--- CPP/CPP.cpp
+++ CPP/CPP.cpp
@@ -2,11 +2,25 @@
 //
 
 #include "stdafx.h"
-#include "CareerCup\ConstructTreeFromInorderAndPreorder.h"
+#include "leetcode\SudokuSolver.h"
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv [])
 {
+    vector<string> input = {
+        "..5..7.13",
+        "3.2......",
+        "....8..9.",
+        ".........",
+        "5..6..4..",
+        "....9...4",
+        "..4..5...",
+        ".5...8...",
+        "........9"
+    };
+
+    cout << SudokuSolver::valid(input, 0, 0, 2);
+
     return 0;
 }
--- CPP/Include/leetcode/SudokuSolver.h
+++ CPP/Include/leetcode/SudokuSolver.h
@@ -1,9 +1,51 @@
 #pragma once
 #include "stdafx.h"
 
+namespace ValidSudoku
+{
+    // Given a sodoku, check if it is a valid one (no duplicates in row, in column and in 3X3 box).
+    bool isValid(vector<string>& board)
+    {
+
+    }
+}
 namespace SudokuSolver
 {
-    void solveSudoku(vector<vector<char>>& board) 
+    bool valid(vector<string>& board, int x, int y, int val)
+    {
+        int m = board.size();
+        int n = board[0].size();
+        // column
+        for (int i = 0; i < m; i++) 
+        {
+            if (board[i][y] != '.' && board[i][y] == val + '0')
+                return false;
+        }
+
+        // row
+        for (int j = 0; j < n; j++)
+        {
+            if (board[x][j] != '.' && board[x][j] == val + '0')
+                return false;
+        }
+
+        //box
+        int square_x = x / 3;
+        int square_y = y / 3;
+        for (int i = square_x * 3; i <= square_x * 3 + 2; i++)
+        {
+            for (int j = square_y * 3; j <= square_y * 3 + 2; j++)
+            {
+                cout << board[i][j] << endl;
+                if (board[i][j] == val + '0')
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    void solveSudoku(vector<string>& board) 
     {
 
     }
