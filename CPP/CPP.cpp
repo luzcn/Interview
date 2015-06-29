de27f1a2e5d041753bf0c55617aca5aa90a0a887
luzcn <luzcn@hotmail.com>
2015-06-29 11:08:55 -0700

    Added singleton design pattern

--- CPP/CPP.cpp
+++ CPP/CPP.cpp
@@ -2,15 +2,13 @@
 //
 
 #include "stdafx.h"
-#include "CareerCup\ClosestPairs.h"
+#include "DesignPattern\Singleton.h"
 using namespace std;
 
 
 
 int _tmain(int argc, _TCHAR* argv [])
 {
-    vector<Point> p{ { 2, 3 }, { 12, 30 }, { 40, 50 }, { 5, 1 }, { 12, 10 }, { 3, 4 } };
-    cout << careercup::closestPair(p) << endl;
-
+    auto& p = DesignPattern::SingletonObject::getInstance();
     return 0;
 }
--- CPP/Include/DesignPattern/Singleton.h
+++ CPP/Include/DesignPattern/Singleton.h
@@ -2,5 +2,24 @@
 
 namespace DesignPattern
 {
+    class SingletonObject
+    {
+    private:
+        // Private Constructor
+        SingletonObject();
+        // Stop the compiler generating methods of copy the object
+        SingletonObject(const SingletonObject& copy);            // Not Implemented
+        SingletonObject& operator=(const SingletonObject& copy); // Not Implemented
 
+    public:
+        static SingletonObject& getInstance()
+        {
+            // The only instance
+            // Guaranteed to be lazy initialized
+            // Guaranteed that it will be destroyed correctly
+            static SingletonObject instance;
+            return instance;
+        }
+    };
 }
+
