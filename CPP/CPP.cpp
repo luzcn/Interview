152baa6be713e8b7c1e1848d230fce590eb9daa4
Zheng Lu <luzcn@hotmail.com>
2015-07-24 16:20:33 -0700

    Updated PathSum

--- CPP/CPP.cpp
+++ CPP/CPP.cpp
@@ -5,13 +5,46 @@
 #include "leetcode\ContainsDuplicate.h"
 
 using namespace std;
+bool isValid(vector<int> current, int r)
+{
+    for (int i = 0; i < r; i++)
+    {
+        if (current[i] == current[r]) // in same row
+            return false;
 
+        if (abs(current[i] - current[r]) == abs(i - r))
+            return false;
+    }
+    return true;
+}
+void solve(vector<vector<int>>& res, vector<int> current, int col, int n)
+{
+    if (col == n)
+    {
+        res.push_back(current);
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        current[col] = i;
+        if (isValid(current, col))
+        {
+            solve(res, current, col + 1, n);
+        }
+    }
+}
+
+vector<vector<int>> solveQueen(int n)
+{
+    vector<vector<int>> res;
+    vector<int> current(n);
+    solve(res, current, 0, n);
+    return res;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    //[-1,-1], 1, -1
-    vector<int> v{ 7,1,3 };
-    cout << leetcode::containsNearbyAlmostDuplicate(v,2,3);
-
+    solveQueen(4);
     return 0;
 }
