26621f23e017d1cb91acf15dd00bbe08e529bc14
Zheng Lu <luzcn@hotmail.com>
2015-07-28 15:19:28 -0700

    Added KMP string matching algorithm.

--- CPP/CPP.cpp
+++ CPP/CPP.cpp
@@ -2,60 +2,13 @@
 //
 
 #include "stdafx.h"
+#include "CareerCup\PatternMatching.h"
 
 using namespace std;
 
-vector<vector<int>> generateMatrix(int n)
-{
-    int value = 1;
-    vector<vector<int>> M(n, vector<int>(n));
 
-    int r1 = 0;
-    int r2 = n - 1;
-
-    int c1 = 0;
-    int c2 = n - 1;
-
-    while (r1 <= r2 && c1 <= c2)
-    {
-        // top row
-        for (int j = c1; j <= c2; j++)
-            M[r1][j] = value++;
-        r1++;
-        if (r1 > r2)  // if n==1
-            break;
-
-        // right column
-        for (int i = r1; i <= r2; i++)
-            M[i][c2] = value++;
-        c2--;
-
-        // bottom row
-        for (int j = c2; j >= c1; j--)
-            M[r2][j] = value++;
-        r2--;
-
-        // left column
-        for (int i = r2; i >= r1; i--)
-            M[i][c1] = value++;
-        c1++;
-    }
-
-    return M;
-}
 int _tmain(int argc, _TCHAR* argv[])
 {
-    //auto t = generateMatrix(1);
-    auto t = generateMatrix(20);
-
-    for (auto v : t)
-    {
-        for (auto i : v)
-        {
-            cout << i << " ";
-        }
-        cout << endl;
-    }
-
+    cout << careercup::KMP::kmp("bbabaabcdaab", "abaab");
     return 0;
 }
