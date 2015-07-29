409e725bba8598a2f4a61f3ef7dfd9dfee1434d3
Zheng Lu <luzcn@hotmail.com>
2015-07-29 13:44:56 -0700

    Added Valid Palindrome string problem.

--- CPP/CPP.cpp
+++ CPP/CPP.cpp
@@ -2,13 +2,35 @@
 //
 
 #include "stdafx.h"
-#include "CareerCup\PatternMatching.h"
+#include <unordered_set>
 
 using namespace std;
 
+bool isHappy(int n)
+{
+    std::unordered_set<int> set;
+    while (set.find(n) == set.end())
+    {
+        if (n == 1)
+            return true;
 
+        set.emplace(n);
+
+        // compute sqrt of each digit
+        int sum = 0;
+        while (n > 0)
+        {
+            auto t = n % 10;
+            n = n / 10;
+            sum += t*t;
+        }
+        n = sum;
+    }
+
+    return false;
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
-    cout << careercup::KMP::kmp("bbabaabcdaab", "abaab");
+    
     return 0;
 }
