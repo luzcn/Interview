a18c0f1d44f235b93059b2adeac36e62eda091d5
Zheng Lu <luzcn@hotmail.com>
2015-07-29 20:46:23 -0700

    Added multiply strings problem

--- CPP/CPP.cpp
+++ CPP/CPP.cpp
@@ -3,34 +3,12 @@
 
 #include "stdafx.h"
 #include <unordered_set>
-
+#include "leetcode\MultiplyStrings.h"
 using namespace std;
 
-bool isHappy(int n)
-{
-    std::unordered_set<int> set;
-    while (set.find(n) == set.end())
-    {
-        if (n == 1)
-            return true;
-
-        set.emplace(n);
-
-        // compute sqrt of each digit
-        int sum = 0;
-        while (n > 0)
-        {
-            auto t = n % 10;
-            n = n / 10;
-            sum += t*t;
-        }
-        n = sum;
-    }
-
-    return false;
-}
 int _tmain(int argc, _TCHAR* argv[])
 {
-    
+    auto t = leetcode::multiply("9","9");
+
     return 0;
 }
