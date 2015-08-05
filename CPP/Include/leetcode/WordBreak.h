8466d9775edbdb43c64dc614614bd6ab1482f10b
Zheng Lu <luzcn@hotmail.com>
2015-08-05 12:07:24 -0700

    Finished restor IP Address problem.

--- CPP/Include/leetcode/WordBreak.h
+++ CPP/Include/leetcode/WordBreak.h
@@ -1,19 +1,17 @@
 #pragma once
 #include <unordered_set>
 
-/***
-*	Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
-
-For example, given
-s = "leetcode",
-dict = ["leet", "code"].
-
-Return true because "leetcode" can be segmented as "leet code".
-*/
+//  Given a string s and a dictionary of words dict, 
+//  determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+//
+//  For example, given
+//  s = "leetcode",
+//  dict = ["leet", "code"].
+//
+//  Return true because "leetcode" can be segmented as "leet code".
 namespace WordBreak
 {
     using namespace std;
-
 
     // The recursive bruteforce solution
     // start from the begining of the input string, find all the substring s.substr(0,i). 
