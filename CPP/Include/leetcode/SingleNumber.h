fb9ffcfa07a4278c83098004cd655e3920988eef
Zheng Lu <luzcn@hotmail.com>
2015-08-10 14:02:16 -0700

    Finished Single Number 3 problem.

--- CPP/Include/leetcode/SingleNumber.h
+++ CPP/Include/leetcode/SingleNumber.h
@@ -42,57 +42,99 @@ namespace leetcode
         return res;
     }
 
+    // SingleNumber 3
+    //  Given 2 * n + 2 numbers, every numbers occurs twice except two, find them.
+    //  Given[1, 2, 2, 3, 4, 4, 5, 3] return 1 and 5
+    // Thought:
+    // 1. Assume the single numbers are x and y, if we xor all the elements, we got t = x^y.
+    // 2. since in xor result, each bit 1 means different bit in x and y, 
+    //  we can find the right most (least significant) 1 to create a mask m.
+    // 3. we can always get tow cases: 
+    //      case1: x&m==0 && y&m!=0
+    //      case2: x&m!= && y&m==0
+    // so we can separate the input array into two parts, each contains one single number.
+    vector<int> singleNumber3(vector<int> &A)
+    {
+        int total_xor = 0;
+        int x = 0;
+        int y = 0;
 
-    //(Idea coming from the internet)
-    //Since we know that XOR operation can be used for testing if 1 bit occurs twice, in other words, for a single bit, if 1 occurs twice, it turns to 0.
-    //Now we need a 3 - time criteria for each bit, by utilizing the bit operations.
-    //This 3 - time criteria needs every bit turns to 0 if  '1' occurs three times.
+        for (int i = 0; i < A.size(); i++)
+        {
+            total_xor ^= A[i];
+        }
+        
+        // int set_bit_no = total_xor&~(total_xor-1);
+        int set_bit_no = 1;
+        while (!(set_bit_no&total_xor))
+        {
+            set_bit_no <<= 1;
+        }
 
-    //If we know on which bits '1' occurs twice, and also know on which bits '1' occurs 1 - time, 
-    //a simple '&' operation would result in the bit where '1' occurs three times.Then we turn these bit to zero, 
-    //would do well for this problem.
+        for (int i = 0; i < A.size(); i++)
+        {
+            if (A[i] & set_bit_no)
+                x ^= A[i];
+            else
+                y ^= A[i];
+        }
 
-    //(1).Check bits which have 1 - time '1', use the XOR operation.
-    //(2).Check bits which have 2 - times '1's, use current 1 - time result & current number.
-    //(3).Check bits which have 3 - times '1's, use '1-time' result & '2-times' result
-    //(4).To turn 3 - times bits into 0:   ~(3 - times result) & 1 - time result
-    //~(3 - times result) & 2 - times result
-
-    //E.g., We have numbers : 101101, 001100, 101010
-    //To count the occurrence of 1's:
-    //101101
-    //001100
-    //101010
-    //count : {2, 0, 3, 2, 1, 1}
-
-    //Denote:
-    //t1: bit = 1 if current bit has 1 - time '1'
-    //t2 : bit = 1 if current bit  has 2 - times '1'
-    //t3 : bit = 1 if current bit  has 3 - times '1'
-
-    //Result :
-    //t1 = 000011, t2 = 100100, t3 = 001000
-
-    //Initialization : t1 = 000000, t2 = 000000, t3 = 000000
-    //(1) 101101
-    //t1 = 101101  (using XOR)
-    //t2 = 000000
-    //t3 = 000000
-
-    //(2)001100
-    //% Current 2 times bits(t2) and NEW 2 times bits coming from 1 time bits and new number.
-    //t2 = t2 | 001100 & t1 = 001100 & 101101 = 001100
-    //t1 = t1 XOR 001100 = 100001
-    //t3 = t2 & t1 = 000000
-
-    //(3)101010
-    //t2 = t2 | (101010 & t1) = t2 | (101010 & 100001) = 101100
-    //t1 = t1 XOR 101010 = 100001 XOR 101010 = 001011
-
-    //t3 = t1 & t2 = 001000
-
-    //% Turn 3 - time bits into zeros
-    //t1 = t1 & ~t3 = 000011
-    //t2 = t2 & ~t3 = 100100
-    
+        vector<int> res;
+        res.push_back(x);
+        res.push_back(y);
+        return res;
+    }
 }
+#if 0
+//(Idea coming from the internet)
+//Since we know that XOR operation can be used for testing if 1 bit occurs twice, in other words, for a single bit, if 1 occurs twice, it turns to 0.
+//Now we need a 3 - time criteria for each bit, by utilizing the bit operations.
+//This 3 - time criteria needs every bit turns to 0 if  '1' occurs three times.
+
+//If we know on which bits '1' occurs twice, and also know on which bits '1' occurs 1 - time, 
+//a simple '&' operation would result in the bit where '1' occurs three times.Then we turn these bit to zero, 
+//would do well for this problem.
+
+//(1).Check bits which have 1 - time '1', use the XOR operation.
+//(2).Check bits which have 2 - times '1's, use current 1 - time result & current number.
+//(3).Check bits which have 3 - times '1's, use '1-time' result & '2-times' result
+//(4).To turn 3 - times bits into 0:   ~(3 - times result) & 1 - time result
+//~(3 - times result) & 2 - times result
+
+//E.g., We have numbers : 101101, 001100, 101010
+//To count the occurrence of 1's:
+//101101
+//001100
+//101010
+//count : {2, 0, 3, 2, 1, 1}
+
+//Denote:
+//t1: bit = 1 if current bit has 1 - time '1'
+//t2 : bit = 1 if current bit  has 2 - times '1'
+//t3 : bit = 1 if current bit  has 3 - times '1'
+
+//Result :
+//t1 = 000011, t2 = 100100, t3 = 001000
+
+//Initialization : t1 = 000000, t2 = 000000, t3 = 000000
+//(1) 101101
+//t1 = 101101  (using XOR)
+//t2 = 000000
+//t3 = 000000
+
+//(2)001100
+//% Current 2 times bits(t2) and NEW 2 times bits coming from 1 time bits and new number.
+//t2 = t2 | 001100 & t1 = 001100 & 101101 = 001100
+//t1 = t1 XOR 001100 = 100001
+//t3 = t2 & t1 = 000000
+
+//(3)101010
+//t2 = t2 | (101010 & t1) = t2 | (101010 & 100001) = 101100
+//t1 = t1 XOR 101010 = 100001 XOR 101010 = 001011
+
+//t3 = t1 & t2 = 001000
+
+//% Turn 3 - time bits into zeros
+//t1 = t1 & ~t3 = 000011
+//t2 = t2 & ~t3 = 100100
+#endif
