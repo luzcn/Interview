19f4626a8201e9733a7be7f04d0337e3c00cd1a7
Zheng Lu <luzcn@hotmail.com>
2015-10-15 09:05:52 -0700

    Need to finish PaintersPatritin DP solution.

--- CPP/Include/CareerCup/PaintersPartition.h
+++ CPP/Include/CareerCup/PaintersPartition.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "stdafx.h"
+#include <numeric>
 
-//You have to paint N boards of length{ A0, A1, A2 … AN - 1 }.
+//You have to paint N boards of length{ A0, A1, A2 ... AN - 1 }.
 //There are K painters available and you are also given how much time a painter takes to paint 1 unit of board.
 //
 //You have to get this job done as soon as possible under the constraints 
@@ -12,5 +13,41 @@
 // Divide A into k or fewer partitions, such that the maximum sum over all the partitions is minimized.
 namespace careercup
 {
-    // TODO
+    int sum(vector<int>& A, int start, int end)
+    {
+        int total = 0;
+        for (int i = start; i <= end; i++)
+        {
+            total += A[i];
+        }
+
+        return total;
+    }
+
+    int partitionRec(vector<int>& A, int n, int k)
+    {
+        if (k == 1)
+            return sum(A, 0, n - 1);
+        if (n == 1)
+            return A[0];
+
+        int best = INT_MAX;
+        for (int j = 1; j <= n; j++)
+        {
+            int lastSum = sum(A, j, n - 1);
+            int leftOPT = partitionRec(A, j, k - 1);
+
+            best = min(best, max(leftOPT, lastSum));
+        }
+
+        return best;
+    }
+
+    int partition(vector<int> boards, int k)
+    {
+        return partitionRec(boards, boards.size(), k);
+    }
+
+
+    // DP solution
 }
