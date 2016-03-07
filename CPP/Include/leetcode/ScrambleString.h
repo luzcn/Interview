65dc708aa4fc10c81170811253007826483401ae
Zheng Lu <luzcn@hotmail.com>
2016-03-06 16:30:24 -0800

    Finished ScrambleString

--- CPP/Include/leetcode/ScrambleString.h
+++ CPP/Include/leetcode/ScrambleString.h
@@ -9,5 +9,56 @@
 
 namespace leetcode
 {
+    bool isScramble(string s1, string s2)
+    {
+        if (s1.size() != s2.size())
+            return false;
 
+        int check[26]{ 0 };
+
+        for (char c : s1)
+        {
+            check[c - 'a']++;
+        }
+
+        for (char c : s2)
+        {
+            check[c - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (check[i] != 0)
+                return false;
+        }
+
+        if (s1.size() == 1 && s2.size() == 1)
+            return true;
+
+        for (int i = 1; i < s1.size(); i++)
+        {
+            string s11 = s1.substr(0, i);
+            string s12 = s1.substr(i);
+
+            string s21 = s2.substr(0, i);
+            string s22 = s2.substr(i);
+
+            if (isScramble(s11, s21) && isScramble(s12, s22))
+            {
+                return true;
+            }
+
+            // already scrambled
+            // for example: "great" and "eatgr"
+            // need to check s1.substr(0, i) and s2.(s2.size()-i, i)
+            s21 = s2.substr(s2.size() - i, i);
+            s22 = s2.substr(0, s2.size() - i);
+            if (isScramble(s11, s21) && isScramble(s12, s22))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
